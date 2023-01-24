0dc5ffda6fb81c2b857d8575bea52d4cd35ec826
deemaaldahrouj <100377483+deemaaldahrouj@users.noreply.github.com>
2023-01-24 09:47:58 +0300

    javascript

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
+const d=new Date();
 function setup(){
 const weekday=["الاحد","الاثنين","الثلاثاء","الاربعاء","الخميس","الجمعة","السبت"];
-const d=new Date();
+
 var day=weekday[d.getDay()];
 day1.innerHTML=day;
 // document.getElementById("day1").style.color= "red";
@@ -9,9 +10,22 @@ day1.innerHTML=day;
 // document.getElementById(curDay).style.background = "#ff651a";
 
 setup();
-
+var vIst=0;
 let num = document.getElementById("num");
 let value = 0;
+function click11(obj)
+ { vIst++;
+   
+  value++;
+   num.textContent = value;
+  if(vIst>=10)
+   {obj.style.background="#ffffff";
+   obj.style.transition="20s";}
+   else
+   {obj.style.background="#3d2525";
+   obj.style.transition="1s";
+   }
+}
 function click1(obj)
  {
    value++;
@@ -26,13 +40,27 @@ function cl(obj){
     if(value>=10)
     {alert(" لقد اتممت الذكر تقبل الله")
      num.textContent=0;
-    value=0;}
+    value=0;
+     }
     else
     {alert("عذرا انك لم تكمل هدف الذكر و هو 100 تسبيحة على الاقل");
 }
+}
 
+var t=0;
+function disp(obj)
+{ 
+  if(t%2==0)
+{document.getElementById("baqyat").style.display="flex";
+obj.style.color="#d35252";
+t++;}
+else
+{document.getElementById("baqyat").style.display="none";
+obj.style.color="rgb(43, 7, 13)";
+t++;}
 
 }
+
 
 // function c2(obj){
 //     vm++;
@@ -46,4 +74,31 @@ function cl(obj){
 // }
 
 
+function daily()
+{
+  switch (d.getDay()) {
+		case 0:
+			document.getElementById("id1").style.display="flex";
+			break;
+		case 1:
+			document.getElementById("id2").style.display="flex";
+			break;
+		case 2:
+			document.getElementById("id3").style.display="flex";
+			break;
+		case 3:
+			document.getElementById("id4").style.display="flex";
+			break;
+      case 4:
+        document.getElementById("id5").style.display="flex";
+			break;
+      case 5:
+        document.getElementById("id6").style.display="flex";
+			break;
+      case 6:
+        document.getElementById("id7").style.display="flex";
+			break;
+	}
 
+}
+daily();
